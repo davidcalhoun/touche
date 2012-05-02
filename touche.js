fc20ccb30de1eba84bd4a3d021fabfcb3bee2d9a
davidcalhoun <david.b.calhoun@gmail.com>
2012-05-02 13:17:25 -0700

    refactor - sandbox code better, return early, better organization for scoped variables

--- touche.js
+++ touche.js
@@ -1,29 +1,41 @@
-// shouldn't need to wait DOM readiness (famous last words...)
-if(!('ontouchstart' in window)) {
-  // keep track of the button state.  we don't want to fire touchmove when the mouse is up
-  var isMouseDown = false;
+// Touché: bringing touch events to non-touch browsers https://github.com/davidcalhoun/touche
+(function(){  // shouldn't need to wait DOM readiness (famous last words...)
+  if('ontouchstart' in window) {
+    // looks like touch events are already present, so return early
+    return;
+  }
+
+  var isMouseDown = false, // so we don't fire touchmove when the mouse is up
+      originator,
+      fireTouch,
+      mousedown,
+      mousemove,
+      mouseup;
   
-  var originator = null;
-  var fireTouch = function(type, e) {
-    var target = originator || e.target;
-    var newEvent = document.createEvent('MouseEvent');  // trying to create an actual TouchEvent will create an error
+  fireTouch = function(type, e) {
+    var target,
+        newEvent,
+        touchesObj;
+
+    target = originator || e.target;
+    newEvent = document.createEvent('MouseEvent');  // trying to create an actual TouchEvent will create an error
     newEvent.initMouseEvent(type, true, true, window, e.detail,
                             e.screenX, e.screenY, e.clientX, e.clientY,
                             e.ctrlKey, e.shiftKey, e.altKey, e.metaKey,
                             e.button, e.relatedTarget);
     
     // touches/targetTouches/changedTouches emulation
-    var touchesObj = [{
+    touchesObj = [{
       // identifier: unique id for the touch event (lazy.. just hooking it into the timestamp)
       // not using Date.now() just for greater support
       identifier: (new Date()).getTime(),
-      pageX: e.pageX,
-      pageY: e.pageY,
-      clientX: e.clientX,
-      clientY: e.clientY,
-      target: target,
-      screenX: e.screenX,
-      screenY: e.screenY
+      pageX:      e.pageX,
+      pageY:      e.pageY,
+      clientX:    e.clientX,
+      clientY:    e.clientY,
+      target:     target,
+      screenX:    e.screenX,
+      screenY:    e.screenY
     }];
 
     switch(type) {
@@ -49,15 +61,15 @@ if(!('ontouchstart' in window)) {
   }
   
   // hook up the mouse->touch mapped listeners
-  var mousedown = function(e) {
+  mousedown = function(e) {
     isMouseDown = true;
     fireTouch('touchstart', e);
   }
-  var mousemove = function(e) {
+  mousemove = function(e) {
     if(!isMouseDown) return;
     fireTouch('touchmove', e);
   }
-  var mouseup = function(e) {
+  mouseup = function(e) {
     isMouseDown = false;
     fireTouch('touchend', e);
   }
@@ -66,7 +78,8 @@ if(!('ontouchstart' in window)) {
   document.addEventListener('mouseup', mouseup, false);
   
   // old style handlers - only here to get around feature detection (comment if you need to)
-  window['ontouchstart'] = mousedown;
-  window['ontouchmove'] = mousemove;
-  window['ontouchend'] = mouseup;
-}
+  window.ontouchstart = mousedown;
+  window.ontouchmove  = mousemove;
+  window.ontouchend   = mouseup;
+
+})();
